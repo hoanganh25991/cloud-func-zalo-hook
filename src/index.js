084daa4c3bc3465126496c3383a16209568f007e
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-15 13:06:49 +0700

    Update hook, just send req, dont wait

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const _ = console.log
 
 export const ORDER_EVENT = "orderEvent$"
 
-export const zaloHook = async (req, res) => {
+export const zaloHook = (req, res) => {
   const eventInfo = req.query
   _("[eventInfo]", eventInfo)
 
@@ -12,10 +12,8 @@ export const zaloHook = async (req, res) => {
   // order
   const { fromuid, oaid, message, order, event: eventName } = eventInfo
   const shouldHandle = message || order
-  if (!shouldHandle) {
-    res.send({ msg: "Obmit" })
-    return
-  }
+
+  if (!shouldHandle) return _("[shouldHandle]", shouldHandle)
 
   const query = order ? ORDER_EVENT : message
 
@@ -31,13 +29,6 @@ export const zaloHook = async (req, res) => {
     }
   }
 
-  try {
-    await call({ query, context, zaloEvent: eventInfo })
-  } catch (err) {
-    _("[call ERR]", err)
-    res.send({ msg: "Send event fail" })
-    return
-  }
-
-  res.send({ msg: "Event sent" })
+  call({ query, context, zaloEvent: eventInfo })
+  _("[call] Sent to dialogflow")
 }
